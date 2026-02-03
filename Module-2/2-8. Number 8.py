edbf16d38c861b66d2715d51bef5f1f76bb0f247
gregorywharrison <gregorywharrison@students.abtech.edu>
2026-02-03 14:31:54 -0500

    Add files via upload
    CSC-121 - Module 2 - 2.8 Number 8 - Assignment

---
+++ Module-2/2-8. Number 8.py
@@ -0,0 +1,8 @@
+# Write an addition operation that results in the number 8
+print(5+3)
+# Write a subtraction operation that results in the number 8
+print(10-2)
+# Write an multiplication operation that results in the number 8
+print(2*4)
+# Write a division operation that results in the number 8
+print(16/2)
