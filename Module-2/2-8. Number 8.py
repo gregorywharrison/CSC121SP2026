4c709be90bb68e98bd912c8c54bba8eb565602b4
gregorywharrison <gregorywharrison@students.abtech.edu>
2026-02-03 14:45:08 -0500

    Delete Module-2/2-8. Number 8.py

--- Module-2/2-8. Number 8.py
+++
@@ -1,8 +0,0 @@
-# Write an addition operation that results in the number 8
-print(5+3)
-# Write a subtraction operation that results in the number 8
-print(10-2)
-# Write an multiplication operation that results in the number 8
-print(2*4)
-# Write a division operation that results in the number 8
-print(16/2)
