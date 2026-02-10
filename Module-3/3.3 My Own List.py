2f0e4ecf558361e3078cf5b8de0d3702261e8fb2
gregorywharrison <gregorywharrison@students.abtech.edu>
2026-02-10 01:03:12 -0500

    Delete Module-3/3.3 My Own List.py

--- Module-3/3.3 My Own List.py
+++
@@ -1,9 +0,0 @@
-schwinn_bicycles = ['supersport', 'continental', 'stingray']
-message = f"When I was 17, I rode my Schwinn {schwinn_bicycles[0].title()} from Asheville, N.C. to St. Augustine Fla. and back."
-print(message)
-
-message = f"\nLast year I restored my neighbor's 1968 Schwinn {schwinn_bicycles[1].title()}. It's a cool bike but much heavier than the {schwinn_bicycles[0].title()}."
-print(message)
-
-message = f"\nMy favorite Christmas present ever was a black {schwinn_bicycles[2].title()}, complete with a silver metal-flake banana seat and chopper handlebars!"
-print(message)
