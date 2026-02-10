9d0c4baeb68ea89e9919a020ff46d43fb161e36d
gregorywharrison <gregorywharrison@students.abtech.edu>
2026-02-10 13:30:55 -0500

    Add files via upload
    Module 3 - 3.4 Guest List - Assignment

---
+++ Module-3/3-8. My Updated Guest List - Sorted.py
@@ -0,0 +1,31 @@
+guests = ['david bowie', 'sister rosetta tharpe', 'johan sebastian bach']
+
+message = f"Dear {guests[0].title()}, please join me and some fellow musicians for dinner and drinks in the Biltmore House Music Room this Saturday!"
+print(message)
+
+message = f"\nDear {guests[1].title()}, please join me and some fellow musicians for dinner and drinks in the Biltmore House Music Room this Saturday!"
+print(message)
+
+message = f"\nDear {guests[2].title()}, please join me and some fellow musicians for dinner and drinks in the Biltmore House Music Room this Saturday!"
+print(message)
+
+print("\nI'm happy to say we can use the main dining table and we have space for more guests!")
+
+guests.insert(0, 'janis joplin')
+guests.insert(2, 'louis armstrong')
+guests.append('frederic chopin')
+
+guests.sort()
+
+message = f"\nDear {guests[0].title()}, please join me and some fellow musicians for dinner and drinks in the Biltmore House Music Room this Saturday!"
+print(message)
+message = f"\nDear {guests[1].title()}, please join me and some fellow musicians for dinner and drinks in the Biltmore House Music Room this Saturday!"
+print(message)
+message = f"\nDear {guests[2].title()}, please join me and some fellow musicians for dinner and drinks in the Biltmore House Music Room this Saturday!"
+print(message)
+message = f"\nDear {guests[3].title()}, please join me and some fellow musicians for dinner and drinks in the Biltmore House Music Room this Saturday!"
+print(message)
+message = f"\nDear {guests[4].title()}, please join me and some fellow musicians for dinner and drinks in the Biltmore House Music Room this Saturday!"
+print(message)
+message = f"\nDear {guests[5].title()}, please join me and some fellow musicians for dinner and drinks in the Biltmore House Music Room this Saturday!"
+print(message)
