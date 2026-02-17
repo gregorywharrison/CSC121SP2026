2137835c3c9d36d86f6441bda61fe5d719395c38
gregorywharrison <gregorywharrison@students.abtech.edu>
2026-02-17 11:46:30 -0500

    Add files via upload
    CSC 121 - 4-2. Animals - Assignment

---
+++ Module-4/4-2. Animals.py
@@ -0,0 +1,6 @@
+song_birds = ['cardinal', 'sparrow', 'bluebird']
+for birds in song_birds:
+
+  print(f"Songbirds like the {birds} originated 50 million years ago.")
+
+print("All songbirds sing to express their location to other birds.")
