ca1ff8a96d9e63de640b016743edb6bfcf474163
gregorywharrison <gregorywharrison@students.abtech.edu>
2026-02-17 19:11:50 -0500

    Add files via upload
    CSC-121 - 4-15. Code Review - Assignment

---
+++ Module-4/4-15. Code Review.py
@@ -0,0 +1,30 @@
+dogs = ['collie', 'beagle', 'boxer']
+for dog in dogs:
+	print(f"A {dog} would make a great pet.")
+
+print("Any of these dogs would make a great pet.") 
+
+pizzas = ['pepperoni', 'green pepper', 'bbq chicken']
+for pizza in pizzas:
+	print(pizza)
+
+pizzas = ['pepperoni', 'green pepper', 'bbq chicken']
+for pizza in pizzas:
+	print(f"I like {pizza} pizza.")
+
+print("I really like pizza")
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+	print(magician + "\n")
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+	print(f"{magician.title()}, that was a great trick!\n")
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+	print(f"{magician.title()}, that was a great trick!")
+	print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+print("Thank you, everyone. That was a great magic show!")
