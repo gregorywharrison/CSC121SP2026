24043ebfa00a7c1736fba2f4f3b1872a73fbed3e
gregorywharrison <gregorywharrison@students.abtech.edu>
2026-02-21 00:03:10 -0500

    Add files via upload
    CSC-121 - 5-3. Alien Colors 1 - 3

---
+++ Module-5/5-3. Alien Colors 1 - 3.py
@@ -0,0 +1,30 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+	print("Good shot! You just earned 5 points!")
+elif alien_color == 'yellow':
+	print("Good shot! You just earned 10 points!")
+else:
+	print("Good shot! You just earned 15 points!")
+
+print()
+
+alien_color = 'yellow'
+
+if alien_color == 'green':
+	print("Good shot! You just earned 5 points!")
+elif alien_color == 'yellow':
+	print("Good shot! You just earned 10 points!")
+else:
+	print("Good shot! You just earned 15 points!")
+    
+print()
+
+alien_color = 'red'
+
+if alien_color == 'green':
+	print("Good shot! You just earned 5 points!")
+elif alien_color == 'yellow':
+	print("Good shot! You just earned 10 points!")
+else:
+	print("Good shot! You just earned 15 points!")
