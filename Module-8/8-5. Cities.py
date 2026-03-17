74265c85650e7b950b4df06c7e6cb4648e637243
gregorywharrison <gregorywharrison@students.abtech.edu>
2026-03-17 15:16:46 -0400

    Add files via upload
    CSC-121 - Module 8 - 8.5 Cities Assignment

---
+++ Module-8/8-5. Cities.py
@@ -0,0 +1,6 @@
+def describe_city(city, country='Japan'):
+	print(f"{city} is in {country.title()}.")
+
+describe_city('Tokyo')
+describe_city('Sapporo')
+describe_city(city='Saigon', country='Vietnam')
